942b7bf46a1fd191ecb5d27ff75fbf6c8a8bbfee
Ron Dumalagan <43971216+ronduma@users.noreply.github.com>
2023-09-14 23:40:11 -0400

    more navbar progress

--- jasmin/src/components/Navbar.js
+++ jasmin/src/components/Navbar.js
@@ -7,30 +7,27 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';  
 
 function Navbar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="static" style={{background:"#85C2A7"}} elevation={0}>
+      <Toolbar>
+        <Box
+          component="img"
+          sx={{
+            height:"4vh"
+          }}
+          src={logo}
+        />
+        <div
+          style={{marginLeft:"auto"}}
+        >
+          <Button color="inherit">About Us</Button>
+          <Button color="inherit">Log In</Button>
+          <Button color="inherit">Register</Button>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 }
 
