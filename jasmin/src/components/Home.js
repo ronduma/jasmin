5fb7759eb25be618094c8a6500541a5a815eae2b
Ron Dumalagan <43971216+ronduma@users.noreply.github.com>
2023-09-15 10:17:38 -0400

    home page progress

--- jasmin/src/components/Home.js
+++ jasmin/src/components/Home.js
@@ -1,10 +1,21 @@
 import '../App.css';
 import Navbar from '../components/Navbar';
 
+import Box from '@mui/material/Box';
+
 function Home() {
   return (
     <div className="Home" >
       <Navbar />
+      <Box
+        className="homeBanner"
+        sx={{
+          backgroundColor:"white",
+          py:"15vh"
+        }}
+      >
+        Bringing together patients and psychologists in a seamless way.
+      </Box>
     </div>
   );
 }
