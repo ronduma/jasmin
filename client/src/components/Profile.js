676130f485ba437df506471a2808de35184e8d9a
Colleen Que <quecolleen@gmail.com>
2023-10-26 23:24:14 -0400

    skeleton

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,11 +1,75 @@
-import '../App.css';
-import Navbar from './Navbar';
+import "../App.css";
+import Navbar from "./Navbar";
+import profile_img from "../images/profile.jpg";
 
 function Profile() {
   return (
-    <div className="Profile" >
+    <div>
       <Navbar />
-      <div>Profile</div>
+      <div>
+        <h1>Profile</h1>
+      </div>
+
+      <div class="boxes_container">
+        <div class="patient">
+          <h1>Ron D</h1>
+          <img src={profile_img} alt="Your Image" class="round-image" />
+
+          <div class="list-container">
+            <ul class="list">
+              <li>Age</li>
+
+              <li>Gender</li>
+              <li>Languages</li>
+              <li>Location</li>
+              <li>Occupation</li>
+            </ul>
+            <ul class="list">
+              <li>21</li>
+              <li>Male</li>
+              <li>English, Viet, Spanish</li>
+              <li>New Jersey, USA</li>
+              <li>Student</li>
+            </ul>
+          </div>
+        </div>
+
+        <button>Edit Profile</button>
+
+        <div class="right-boxes">
+          <div class="right-box">
+            <h1>Bio</h1>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+              felis tellus, malesuada vel cursus et, sodales sit amet eros.
+              Integer sed justo ac dolor molestie.
+            </p>
+          </div>
+
+          <div class="right-box">
+            <h1>Core Concerns</h1>
+            <ul>
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            </ul>
+          </div>
+
+          <div class="right-box">
+            <h1>Frustrations</h1>
+            <ul>
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
