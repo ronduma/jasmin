e9717614145784e100fe77c9601f06a2902912fa
Jae <84739325+jaejang02@users.noreply.github.com>
2023-11-13 16:25:15 -0500

    Fixed Profile Information
    Needed to set a if and else for profile information as profileData was set as undefined

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -7,7 +7,6 @@ const xss = require('xss');
 
 router.get('/:id', async(req,res) => {
     const userObject = await users.getUserById(req.params.id);
-    console.log(userObject);
     return res.status(200).json(userObject);
 })
 router.post('/', async(req,res) => {
