2dd239ac3372fb3ae2740d897c71feb54d71d04d
Ron Dumalagan <43971216+ronduma@users.noreply.github.com>
2024-04-02 16:18:18 -0400

    removed navbar redundancies

--- client/src/components/navbar/Navigation.jsx
+++ client/src/components/navbar/Navigation.jsx
@@ -7,19 +7,13 @@ import './styles.css';
 import { CircleUserRound } from 'lucide-react';
 
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 
 const Navigation = () => {
   const {currentUser} = useContext(AuthContext);
-  return <div>{currentUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>;
-};
-
-const NavigationAuth = () => {
-
   return (
-    <AppBar position="static" style={{background:"#F2F0E1", margin:'2vh 0 0 0'}} elevation={0}>
+    <AppBar position="static" style={{background:"#F2F0E1", margin:'2vh 0 1vh 0'}} elevation={0}>
       <Toolbar>
         <Button component = {NavLink} to='/' color='green' className="navlink" sx={{fontSize:"20pt", fontWeight:"800"}}>
           <img
@@ -30,46 +24,33 @@ const NavigationAuth = () => {
             }}
           />
         </Button>
-        <div
-          style={{marginLeft:"auto"}}
-        >
-          <Button component = {NavLink} to='/about' color='green' className="navlink">Personal Therapy</Button>
-          <Button component = {NavLink} to='/about' color='green' className="navlink">Couples Therapy</Button>
-          <Button component = {NavLink} to='/about' color='green' className="navlink">Children Therapy</Button>
-          <Button component = {NavLink} to='/profile' color='green' className="navlink"><CircleUserRound/></Button>
-          <LogOutButton 
-            style={{
-              height:'1em'
-            }}
-          />
+        <div style={{marginLeft:"auto"}}>
+          {currentUser ? <NavigationAuth /> : <NavigationNonAuth />}
         </div>
       </Toolbar>
     </AppBar>
   );
 };
 
+const NavigationAuth = () => {
+  return (
+    <div>
+      <Button component = {NavLink} to='/' color='green' className="navlink">Personal Therapy</Button>
+      <Button component = {NavLink} to='/' color='green' className="navlink">Couples Therapy</Button>
+      <Button component = {NavLink} to='/' color='green' className="navlink">Children Therapy</Button>
+      <Button component = {NavLink} to='/profile' color='green' className="navlink"><CircleUserRound/></Button>
+      <LogOutButton style={{ height:'1em' }}/>
+    </div>
+  );
+};
+
 const NavigationNonAuth = () => {
   return (
-    <AppBar position="static" style={{background:"#F2F0E1", margin:'2vh 0 0 0'}} elevation={0}>
-    <Toolbar>
-      <Button component = {NavLink} to='/' color='green' className="navlink" sx={{fontSize:"20pt", fontWeight:"800"}}>
-        <img
-          alt='google signin'
-          src='/imgs/logo.png'
-          style={{
-            height:'2em'
-          }}
-        /> 
-      </Button>
-      <div
-        style={{marginLeft:"auto"}}
-      >
+    <div>
         <Button component = {NavLink} to='/about' color="green">About Us</Button>
         <Button component = {NavLink} to='/register' color="green">Register</Button>
         <Button component = {NavLink} to='/login' color="green">Log In</Button>
-      </div>
-    </Toolbar>
-  </AppBar>
+    </div>
   );
 };
 
