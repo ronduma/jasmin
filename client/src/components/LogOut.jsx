53c3f47b884ae38c428678380e3794db165f34b8
bishawjitsaha <87948050+bishawjitsaha@users.noreply.github.com>
2024-04-02 21:05:10 -0400

    PyschologistView for matching
    Also:
     - Added icon for kAI chat
     - Made Logout also redirect to homepage

--- client/src/components/LogOut.jsx
+++ client/src/components/LogOut.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {doSignOut} from '../firebase/FirebaseFunctions';
 import Button from '@mui/material/Button';
+import {NavLink} from 'react-router-dom';
 
 import '../App.css';
 
 const LogOutButton = () => {
   return (
-    <Button onClick={doSignOut} color='green'>
+    <Button component = {NavLink} to='/' onClick={doSignOut} color='green'>
       Log Out
     </Button>
   );
