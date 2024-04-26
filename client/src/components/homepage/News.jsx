5ee1744906859b62a198946e5881106df1f20dc5
Ron Dumalagan <43971216+ronduma@users.noreply.github.com>
2024-04-25 23:38:49 -0400

    card stuff

--- client/src/components/homepage/News.jsx
+++ client/src/components/homepage/News.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './styles.css';
 
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
 import Grid from '@mui/material/Grid';
 
 // NOTE: these are for demo purposes, to be populated with backend eventually
@@ -31,23 +33,33 @@ const News = () => {
         <Grid item xs={12} className="reviews-text">
           Latest news
         </Grid>
-        <Grid container spacing={2}>
+        <Grid 
+          container 
+          direction="row"
+          justifyContent="center"
+          alignItems="stretch"
+          spacing={2}
+        >
           {news.map((article, index) => (
             <Grid key={index} item xs={4}>
-              <div className="image-container">
-                <img src={article.url} alt={`Image ${index + 1}`} className="types-image" />
-              </div>
-              <div className="detail-container"> 
-                <div className="news-label">
-                  {article.label}
-                </div>
-                <div className="news-title">
-                  {article.title}
-                </div>
-                <div className="types-caption">
-                  {article.caption}
-                </div>
-              </div>
+              <Card style={{height:"100%", padding:"2rem"}}>
+                <CardActionArea >
+                  <div className="image-container">
+                    <img src={article.url} alt={`Image ${index + 1}`} className="types-image" />
+                  </div>
+                  <div className="detail-container"> 
+                    <div className="news-label">
+                      {article.label}
+                    </div>
+                    <div className="news-title">
+                      {article.title}
+                    </div>
+                    <div className="types-caption">
+                      {article.caption}
+                    </div>
+                  </div>
+                </CardActionArea>
+              </Card>
             </Grid>
           ))}
         </Grid>
