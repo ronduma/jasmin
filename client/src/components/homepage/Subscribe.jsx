d673ad6ef981fdfaa77cf20cf395e95b5c572282
Ron Dumalagan <43971216+ronduma@users.noreply.github.com>
2024-04-25 23:30:07 -0400

    subscribe updates

--- client/src/components/homepage/Subscribe.jsx
+++ client/src/components/homepage/Subscribe.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './styles.css';
 
+import { NavLink } from 'react-router-dom';
+import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 
 import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
@@ -18,14 +20,22 @@ const Subscribe = () => {
         className="stats-container"
       > 
         <Grid item xs={12} className="reviews-text">
-          Join our healing community. Subscribe today.
+          Join our healing community. Register today.
         </Grid>
-        <Grid item xs={12} className="reviews-text">
+        <Grid 
+          item 
+          xs={12} 
+          className="reviews-text" 
+        >
           <TextField id="outlined-basic" label="Email" variant="outlined" className="reviews-input" />
-          <ArrowCircleRightOutlinedIcon className="reviews-btn"/>
+          <Button component={NavLink} to='/register' color='green'
+            style={{padding:"0 0 1.5rem 0"}}
+          >
+            <ArrowCircleRightOutlinedIcon className="reviews-btn"/>
+          </Button>
         </Grid>
         <Grid item xs={12} className="reviews-subtext">
-          Subscribe for updates, news, events, and community resources.
+          Register for updates, news, events, and community resources.
         </Grid>
       </Grid>
     </div>
